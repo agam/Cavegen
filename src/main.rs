2e66e199332c3af130ea946f9899207c87818e46
Agam <agam.brahma@gmail.com>
2021-01-18 08:50:09 +0000

    Intermediate version that doesn't compile.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // A "translation" from https://bitbucket.org/agambrahma/caves/
 
 use array2d::Array2D;
+use rand::Rng;
 
 #[derive(Debug, Clone, Copy)]
 enum Cell {
@@ -16,12 +17,13 @@ struct Params {
 const NUM_ROWS: usize = 5;
 const NUM_COLS: usize = 7;
 
-const WALL_PROB_PCT: i32 = 40;
+const WALL_PROB_PCT: u8 = 40;
 
 fn main() {
     println!("Hello, world!");
 
-    let mut grid = Array2D::filled_with(Cell::Space, NUM_ROWS, NUM_COLS);
+    let mut grid: Array2D<Cell> = Array2D::filled_with(Cell::Space, NUM_ROWS, NUM_COLS);
+    let mut rng = rand::thread_rng();
 
     // Display.
     let show_grid = || {
@@ -37,5 +39,42 @@ fn main() {
         }
     };
 
+    // TODO: figure out why the "closure form" of this was harder to work with.
+    fn seed_caves(grid: &mut Array2D<Cell>, rng: &mut rand::rngs::ThreadRng) {
+        fn is_edge(row: usize, col: usize) -> bool {
+            (row == 0) ||
+                (col == 0) ||
+                (row == NUM_ROWS - 1) ||
+                (col == NUM_COLS - 1)
+        }
+
+        fn is_middle_row(row: usize) -> bool {
+            row == (NUM_ROWS / 2)
+        }
+
+        fn should_place_wall(generator: &mut rand::rngs::ThreadRng) -> bool {
+            generator.gen::<u8>() < WALL_PROB_PCT
+        }
+
+        for row in 0..NUM_ROWS {
+            for col in 0..NUM_COLS {
+                let newVal =
+                    if is_edge(row, col) {
+                        Cell::Wall
+                    } else if is_middle_row(row) {
+                        Cell::Space
+                    } else if should_place_wall(rng) {
+                        Cell::Wall
+                    } else {
+                        Cell::Space
+                    };
+
+                let result = grid.set(row, col, newVal);
+                assert!(result.is_ok());
+            }
+        }
+    };
+
+    seed_caves(&mut grid, &mut rng);
     show_grid();
 }
